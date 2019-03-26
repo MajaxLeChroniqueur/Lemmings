2857578bbc5e7e2d40b1534cb963ca47a0145cc9
Titouan Mergem <t.mergem@rubika-edu.com>
2019-03-26 10:35:56 +0100

    Système de vue dans le noir [Lemmings remastered]

---
+++ Assets/Scripts/Titouan Folder/StartLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevel : MonoBehaviour {
+
+    [SerializeField]
+    private Camera cameraPresentation;
+
+    [SerializeField]
+    private Animator fadeToBlack;
+
+    [SerializeField]
+    private Animator countToThree;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
