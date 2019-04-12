aa593d850adabfe2c8bb500f2b4d23997ada907a
Timothe <t.lafranche@rubika-edu.com>
2019-04-12 14:52:19 +0200

    GraphDeMerde

---
+++ Assets/Scripts/AntiOOBScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    public class AntiOOBScript : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.tag == "Lemmings")
+            {
+                collision.GetComponent<Unit>().Die();
+            }
+        }
+    }
+}
